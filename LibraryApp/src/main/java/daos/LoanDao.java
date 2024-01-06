8cb9f4d2bd81160e40ffbaa74d353571f7831936
Leo <leozelion31@gmail.com>
2024-01-06 15:40:16 +0000

    Updated the loans dao
    This includes checking of the card details and making the payment for the overdue fees.

--- LibraryApp/src/main/java/daos/LoanDao.java
+++ LibraryApp/src/main/java/daos/LoanDao.java
@@ -7,6 +7,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -152,5 +154,123 @@ public class LoanDao extends Dao{
         }
         return overdueFees;
     }
+
+    /**
+     * validates the credit card number using Luhn algorithm
+     *
+     * this checks credit length and removes any spaces or anything user might put in
+     *
+     *
+     * @param cardNumber the credit card number to validate
+     * @return if credit card number is valid return true otherwise return false
+     */
+    public  boolean isCreditCardValid(String cardNumber) {
+
+        cardNumber = cardNumber.replaceAll("[^0-9]", "");
+
+        int length = cardNumber.length();
+        if (length < 13 || length > 19) {
+            return false;
+        }
+        int sum = 0;
+        boolean doubleDigit = false;
+
+        //Luhn algorithm
+        for (int i = length - 1; i >= 0; i--) {
+            int digit = Character.getNumericValue(cardNumber.charAt(i));
+
+            if (doubleDigit) {
+                digit *= 2;
+                if (digit > 9) {
+                    digit -= 9;
+                }
+            }
+
+            sum += digit;
+            doubleDigit = !doubleDigit;
+        }
+
+        return (sum % 10 == 0);
+    }
+    //leo
+    /**
+     * validates the format and checks if date is correct
+     *
+     * this method checks the format of the date, and it
+     * expires in the future cant be less than the current date
+     *
+     * @param expiryDate
+     * @return
+     */
+    public boolean isValidExpiryDate(String expiryDate) {
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("MM/yyyy");
+            Date currentDate = new Date();
+            Date parsedExpiryDate = dateFormat.parse(expiryDate);
+
+            return currentDate.before(parsedExpiryDate);
+        } catch (ParseException e) {
+            return false;
+        }
+    }
+    
+     //leo
+    /**
+     * checks and processes the payment for a late fee for a member
+     *
+     * this method validates credit card info calling the 2 other methods to check.
+     * once valid then processes payment for late fee. if both are true proceed onwards
+     * otherwise return false.
+     *
+     *
+     * @param memberId id of the member making payment for late fee
+     * @param cardNumber the members credit card number
+     * @param expiryDate the expiry date of the credit card
+     * @return true if the payment was successful and credit card info is correct
+     * @throws RuntimeException if error occurs with payment or database connection
+     */
+    public boolean payLateFeeValidate(int memberId,String cardNumber,String expiryDate) {
+
+        if (!isCreditCardValid(cardNumber)) {
+            return false;
+        }
+        if(!isValidExpiryDate(expiryDate)){
+            return false;
+        }
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            String sql = "UPDATE loans Set LateFee = 0.00 where memberId =?";
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, memberId);
+
+            int rows = ps.executeUpdate();
+
+            return rows>0;
+
+        } catch (DaoException e) {
+            throw new RuntimeException("Error paying late fee: " + e.getMessage());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException e) {
+                System.out.println("Error occurred");
+            }
+
+
+        }
+    }
+
+
   }
 
