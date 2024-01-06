881bef846698ef7bd8854e4b38e5dbb9a6708cdc
Leo <leozelion31@gmail.com>
2024-01-06 18:40:44 +0000

    Created loandao interface
    Created the layout for the interface tests

---
+++ LibraryApp/src/test/java/daos/LoanDaoInterfaceTest.java
@@ -0,0 +1,32 @@
+package daos;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoanDaoInterfaceTest {
+
+    @Test
+    void viewActiveLoans() {
+    }
+
+    @Test
+    void getLoansForMember() {
+    }
+
+    @Test
+    void getCurrentOverdueFees() {
+    }
+
+    @Test
+    void isCreditCardValid() {
+    }
+
+    @Test
+    void isValidExpiryDate() {
+    }
+
+    @Test
+    void payLateFeeValidate() {
+    }
+}
