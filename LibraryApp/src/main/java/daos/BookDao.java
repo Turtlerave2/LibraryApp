3c904a2c36fda3a90d7572e8b9f1764d340678c7
Destiny <146720629+FVGhost@users.noreply.github.com>
2024-01-06 20:01:07 +0000

    made all the methods for book

--- LibraryApp/src/main/java/daos/BookDao.java
+++ LibraryApp/src/main/java/daos/BookDao.java
@@ -14,9 +14,8 @@ import java.util.List;
 
 public class BookDao extends Dao {
 
-    //leo
 
-    /**
+    /** Destiny
      * Constructs a new BookDao with the specified database name.
      *
      * @param databaseName The name of the database to be used by the BookDao.
@@ -25,13 +24,13 @@ public class BookDao extends Dao {
         super(databaseName);
     }
 
-    /**
+    /** Destiny
      * Retrieves a list of all books from the database
      *
      * @return A list of book objects containing info about all the books
      * @throws DaoException if an error occurs in the database operation
      */
-    //leo
+
     public List<Book> findAllBooks() throws DaoException {
         Connection con = null;
         PreparedStatement ps = null;
@@ -76,7 +75,13 @@ public class BookDao extends Dao {
         }
         return books;
     }
-
+    /** Destiny
+     * Searches for a book by title in the database.
+     *
+     * @param title The title of the book to search for.
+     * @return A list of book objects matching the search criteria.
+     * @throws DaoException if an error occurs in the database operation.
+     */
         public List<Book> searchBookTitle(String title) throws DaoException {
             Connection con = null;
             PreparedStatement ps = null;
@@ -96,12 +101,96 @@ public class BookDao extends Dao {
                 while (rs.next()) {
                     int bookID = rs.getInt("Bookid");
                     String bookTitle = rs.getString("Title").trim();
+                    int authorID = rs.getInt("AuthorID");
+                    int ISBN = rs.getInt("ISBN");
+                    int publicationYear = rs.getInt("PublicationYear");
+                    int genreID = rs.getInt("GenreID");
+                    int totalCopies = rs.getInt("TotalCopies");
+                    String description = rs.getString("Description").trim();
 
-
+                    Book book = new Book(bookID, bookTitle, authorID, ISBN, publicationYear, genreID, totalCopies, description);
+                    books.add(book);
 
                 }
-            }
+            } catch (SQLException e) {
+                throw new DaoException("searchBookByTitle" + e.getMessage());
+        } finally {
+                try {
+                    if(rs != null) {
+                        rs.close();
+                    }
+                    if (ps != null) {
+                        freeConnection(con);
+                    }
 
+                } catch (SQLException e) {
+                    throw new DaoException(e.getMessage());
+                }
+            }
+            return books;
+    }
+    /** Destiny
+     * Borrows a book by updating the total copies available in the database.
+     *
+     * @param bookID The ID of the book to be borrowed.
+     * @throws DaoException if an error occurs in the database operation.
+     */
+    public void borrowBook(int bookID) throws DaoException {
+        Connection con = null;
+        PreparedStatement ps = null;
+
+        try {
+            con = getConnection();
+            String query = "UPDATE book SET TotalCopies = TotalCopies -1 WHERE Bookid = ?";
+            ps = con.prepareStatement(query);
+            ps.setInt(1, bookID);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new DaoException("borrowBook()" + e.getMessage());
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+                if (con != null) {
+                    freeConnection(con);
+                }
+            } catch (SQLException e) {
+                throw  new DaoException(e.getMessage());
+
+            }
+        }
+    }
+    /** Destiny
+     * Returns a borrowed book by updating the total copies available in the database.
+     *
+     * @param bookID The ID of the book to be returned.
+     * @throws DaoException if an error occurs in the database operation.
+     */
+    public void returnBook(int bookID) throws DaoException {
+        Connection con = null;
+        PreparedStatement ps = null;
+        try {
+            con = getConnection();
+
+            String query = "UPDATE book SET TotalCopies = TotalCopies +1 WHERE Bookid = ?";
+            ps = con.prepareStatement(query);
+            ps.setInt(1, bookID);
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DaoException("returnBook() " + e.getMessage());
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+                if (con != null) {
+                    freeConnection(con);
+                }
+            } catch (SQLException e) {
+                throw new DaoException(e.getMessage());
+            }
         }
     }
 }
