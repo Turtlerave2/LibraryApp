134194feac944113421dc18fbb0a7bb540fd24e7
Leo <leozelion31@gmail.com>
2024-01-06 14:41:43 +0000

    Updated Interface for loan
    Added 2 written methods..

--- LibraryApp/src/main/java/daos/LoanDaoInterface.java
+++ LibraryApp/src/main/java/daos/LoanDaoInterface.java
@@ -1,5 +1,13 @@
 package daos;
 
-public class LoanDaoInterface {
-    
+import business.Loans;
+
+import java.util.List;
+
+public interface LoanDaoInterface {
+    List<Loans> viewActiveLoans(int memberID);
+
+    List<Loans> getLoansForMember(int memberId);
+
+
 }
